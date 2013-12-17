6181f8f5de89581fb52498c40b0f00ad964cbb9b
Leeroy Brun <leeroy.brun@gmail.com>
2013-12-17 11:54:15 +0100

    Initial commit.

---
+++ tpl/main.js
@@ -0,0 +1,11 @@
+function setup(options, imports, register) {
+	// Your plugin code here
+
+    register(null, {
+        '--pluginName--': {
+            // Register data/objects here
+        }
+    });
+}
+
+module.exports = setup;
